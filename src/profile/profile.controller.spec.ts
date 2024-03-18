8b3315a3349fb841e9b76cee462b4f5904161a9d
EjembiEmmanuel <ejembioche50@gmail.com>
2024-03-18 10:06:52 +0100

    test: create tests for profile controller

--- src/profile/profile.controller.spec.ts
+++ src/profile/profile.controller.spec.ts
@@ -1,18 +1,93 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProfileController } from './profile.controller';
+import { ProfileService } from './profile.service';
+import { ProfileDto } from './dto';
+import { Profile } from '@prisma/client';
+
+const profileServiceMock = {
+  create: jest.fn(),
+  update: jest.fn(),
+  findAll: jest.fn(),
+  findOne: jest.fn(),
+};
 
 describe('ProfileController', () => {
-  let controller: ProfileController;
+  let profileController: ProfileController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ProfileController],
+      providers: [{ provide: ProfileService, useValue: profileServiceMock }],
     }).compile();
 
-    controller = module.get<ProfileController>(ProfileController);
+    profileController = module.get<ProfileController>(ProfileController);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(profileController).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should create a user', async () => {
+      const profile: ProfileDto = {
+        userId: 1,
+        bio: 'EDQBxEdQpejDrnhTwNav',
+      };
+
+      profileServiceMock.create.mockResolvedValue(profile);
+
+      const result = await profileController.create(profile);
+      expect(result).toEqual(profile);
+      expect(profileServiceMock.create).toHaveBeenCalledWith(profile);
+    });
+  });
+
+  describe('update', () => {
+    it('should update a profile', async () => {
+      const profile: ProfileDto = {
+        userId: 1,
+        bio: 'utrYerfHIBZlPOevesFk',
+      };
+
+      profileServiceMock.update.mockResolvedValue(profile);
+
+      const result = await profileController.update(profile);
+      expect(result).toEqual(profile);
+      expect(profileServiceMock.update).toHaveBeenCalledWith(profile);
+    });
+  });
+
+  describe('findAll', () => {
+    it('should return an array of profile', async () => {
+      const profiles: Profile[] = [
+        {
+          userId: 1,
+          bio: 'utrYerfHIBZlPOevesFk',
+          id: 1,
+        },
+      ];
+
+      profileServiceMock.findAll.mockResolvedValue(profiles);
+
+      const result = await profileController.findAll();
+      expect(result).toEqual(profiles);
+      expect(profileServiceMock.findAll).toHaveBeenCalled();
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return a profile', async () => {
+      const profile: Profile = {
+        userId: 1,
+        bio: 'utrYerfHIBZlPOevesFk',
+        id: 1,
+      };
+
+      profileServiceMock.findOne.mockResolvedValue(profile);
+
+      const result = await profileController.findOne(profile.userId);
+      expect(result).toEqual(profile);
+      expect(profileServiceMock.findOne).toHaveBeenCalledWith(profile.userId);
+    });
   });
 });
